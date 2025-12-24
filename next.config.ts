0330a7104aaec810413531136ca6fc4d3e575a34
Gio Romanadze <GiooRomanadze@gmail.com>
2025-12-24 18:16:59 +0400

    init

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    rules: {
+      '*.vert': { loaders: ['raw-loader'], as: '*.js' },
+      '*.frag': { loaders: ['raw-loader'], as: '*.js' },
+      '*.glsl': { loaders: ['raw-loader'], as: '*.js' },
+    },
+  },
 };
 
 export default nextConfig;
--- src/app/(modules)/canvas/(modules)/shaders/(page)/page.tsx
+++ src/app/(modules)/canvas/(modules)/shaders/(page)/page.tsx
@@ -2,6 +2,8 @@
 
 import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import fragmentShader from './parts/fragment/index.frag';
+import vertexShader from './parts/vertex/index.vert';
 
 const First = () => {
   return (
@@ -9,9 +11,8 @@ const First = () => {
       <OrbitControls />
 
       <mesh>
-        <boxGeometry />
-
-        <meshStandardMaterial />
+        <planeGeometry args={[1, 1, 32, 32]} />
+        <rawShaderMaterial vertexShader={vertexShader} fragmentShader={fragmentShader} />
       </mesh>
     </Canvas>
   );
