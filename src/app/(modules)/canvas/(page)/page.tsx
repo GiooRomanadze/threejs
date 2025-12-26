7ac6a5e3c7f80ce0e1832f451da47c62e4ad3ec1
Gio Romanadze <GiooRomanadze@gmail.com>
2025-12-26 09:22:59 +0400

    init

--- src/app/(modules)/canvas/(page)/page.tsx
+++
@@ -1,5 +0,0 @@
-const Canvas = () => {
-  return <h1>Three Canvas Module</h1>;
-};
-
-export default Canvas;
--- src/app/(modules)/canvas/types.ts
+++
@@ -1,4 +0,0 @@
-import type { ReactNode } from 'react';
-
-export type CanvasLayoutProps = { children: ReactNode };
-export type CanvasLayoutFn = (props: Readonly<CanvasLayoutProps>) => ReactNode;
