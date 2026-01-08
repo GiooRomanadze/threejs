fe60cb36ebbb9cc324ca50f300503a1937462e1e
Gio Romanadze <GiooRomanadze@gmail.com>
2026-01-08 15:36:20 +0400

    fix

--- src/app/(page)/types.ts
+++ src/app/(page)/types.ts
@@ -1,14 +1,3 @@
 import type { ReactNode } from 'react';
 
-// id, name, amount, items, onChange
-type HomeProps = {
-  checked: boolean;
-  id: string;
-  name: string;
-  disabled?: boolean;
-  amount: number;
-  itemsArr: string[];
-  itemsObj: { [key: string]: string };
-  onChange: (value: string) => void;
-};
-export type HomeFn = (props: HomeProps) => ReactNode;
+export type HomeFn = () => ReactNode;
